67f6ccbd2403c9a6560b47ec96274ee04abe8213
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2017-07-21 15:52:13 +0900

    first commit (error remains)

---
+++ main.py
@@ -0,0 +1,59 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+
+class SBIHandler:
+    URL = "https://site0.sbisec.co.jp/marble/fund/powersearch/fundpsearch.do"
+
+    def __init__(self):
+        self.browser = webdriver.Chrome()
+        self.browser.get(self.URL)
+
+    def fetch_all(self):
+        result = []
+
+        elems = self.browser.find_elements_by_class_name("fundDetail")
+        for e in elems:
+            result.append(
+                {
+                    'url': e.get_attribute('href'),
+                    'name': e.text,
+                }
+            )
+
+        while(True):
+            pager = self.browser.find_element_by_link_text('次へ→')
+            if pager is None:
+                break;
+            pager.click()
+
+            # marker = WebDriverWait(self.browser, 3).until(lambda x: x.find_element_by_class_name('md-l-table-01-fund'))
+            marker = WebDriverWait(self.browser, 3).until(lambda x: x.find_elements_by_class_name('md-l-tr-03'))
+
+            elems = self.browser.find_elements_by_class_name('fundDetail')
+            for e in elems:
+                result.append(
+                    {
+                        'url': e.get_attribute('href'),
+                        'name': e.text,
+                    }
+                )
+
+        return result
+
+    def open_and_fetch_detail(self, url):
+        pass
+
+    def close(self):
+        self.browser.close()
+
+
+def main():
+    handler = SBIHandler()
+    result = handler.fetch_all()
+    print(result)
+    handler.close()
+
+
+if __name__ == '__main__':
+    main()
